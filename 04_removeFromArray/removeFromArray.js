7144513181a4dfe02017871250014932e5323365
CI <106698151+lurulu00@users.noreply.github.com>
2022-11-11 08:00:49 +0700

    Create draft solution for Ex 04 and work environment
    * created 'working on solutions here.html' to create solutions all
    exercise, from this point forward
    * also passed first test of Ex 04, but need to modify to fulfill
    subsequent tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (inputArray, condition) {
+    for (let i in inputArray) {
+        if (inputArray[i] == condition) {
+            inputArray.splice(i, 1);
+        }
+    }
+    return inputArray;
 };
 
 // Do not edit below this line
