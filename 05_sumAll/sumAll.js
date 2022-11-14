64ad5eada6fe5951f40d2b1403ac8cb2a297f08b
CI <106698151+lurulu00@users.noreply.github.com>
2022-11-14 10:11:03 +0700

    Create solution for Ex 05
    No issues this time. Just knock it out quickly

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+    let sum = 0;
+    if ( start <0 || end < 0 || typeof start != 'number' || typeof end != 'number'){
+        return "ERROR";
+    }
+    if (start > end) {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+    for (start; start <= end; start++) {
+        sum += start;
+    }
+    return sum;
 };
 
 // Do not edit below this line
