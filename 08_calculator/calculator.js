897c79a350ccea43f3a8978444b539d206ef09ff
CI <106698151+lurulu00@users.noreply.github.com>
2022-11-17 11:56:46 +0700

    Create solution for exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function (x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+  let sumOf = 0;
+  numbers.forEach((number) => {
+    sumOf += number;
+  });
+  return sumOf;
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+  let productOf = 1;
+  numbers.forEach((number) => {
+    productOf *= number;
+  });
+  return productOf;
 };
 
-const power = function() {
-	
+const power = function (a,b) {
+  return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  if(number){
+    let result = 1;
+    while(number >0){
+      result *= number;
+      number--;
+    }
+    return result;
+  }
+  return 1;
 };
 
 // Do not edit below this line
