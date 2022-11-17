373a87608066e5d58adcb2af93d75e4737cc0e6b
CI <106698151+lurulu00@users.noreply.github.com>
2022-11-17 13:08:44 +0700

    Create solution for Ex 09 Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+/* const palindromes = function (sentence) {
+    sentence= sentence.replace(/[^A-Za-z]/g, '').toLowerCase(); 
+    // removes punctuation (non alphabet characters), then make them all uniform lowercase
+    
+    reversedSentence = (sentence.split("").reverse().join("")); 
+    //create a comparison string that is the reverse of original
 
+    return sentence===reversedSentence;
+}; */
+
+// made it prettier
+const palindromes = (sentence) => {
+    sentence= sentence.replace(/[^A-Za-z]/g, '').toLowerCase(); 
+    return sentence === (sentence.split("").reverse().join(""));
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
