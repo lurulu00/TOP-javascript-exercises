619be4ef3a86f28041c1db1f5520813c49cfe619
CI <106698151+lurulu00@users.noreply.github.com>
2022-11-17 15:15:50 +0700

    Create solution for Exercise 12 findTheOldest
    Spent way too much time trying to get a more concise and modern solution,
    in the end I stuck with what I know can work. Looking at the solution
    branch on github, I can see now how it works, but it is still not
    intuitive how I'm supposed to get to that solution.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    // add age value to people array
+    people.forEach(person =>{
+        if (isNaN(person.yearOfDeath)){
+          person.age=(new Date()).getFullYear()-person.yearOfBirth;
+        } else {
+          person.age = person.yearOfDeath-person.yearOfBirth;
+        }
+    });
+
+    // create a new array sorted by oldest
+    const oldest = people.sort(function (a,b){
+        return a.age>b.age ? -1 : 1
+      });
+
+
+      return(oldest[0]);
 
 };
+
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
