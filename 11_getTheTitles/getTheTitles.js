2acf5f6d55f039e4941df88a46165df9f8c40a0e
CI <106698151+lurulu00@users.noreply.github.com>
2022-11-17 14:16:06 +0700

    Create solution for exercise 11 getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,13 @@
-const getTheTitles = function() {
+/* const getTheTitles = function (books) {
+    let titles = [];
+    books.forEach(book => titles.push(book.title));
+    return titles;
+}; */
 
+// make prettier
+// forgot map exists 
+const getTheTitles = function (books) {
+    return books.map(book => book.title);;
 };
 
 // Do not edit below this line
