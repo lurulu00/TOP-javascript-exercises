a7b9e0a03f13c0fb06f6293bf7b4208dca490270
CI <106698151+lurulu00@users.noreply.github.com>
2022-11-17 13:58:47 +0700

    Create solution for Ex 10 Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacci = (number) => {
+    let previousNumber = 0,
+        currentNumber =1;
+    if (number<0){
+      return "OOPS";
+    } else {
+      for (let i =1; i <number; i++){
+        let temp=currentNumber;
+        currentNumber += previousNumber;
+        previousNumber=temp;
+      }
+      return currentNumber;
+    } 
+  };
 
-};
 
+  
 // Do not edit below this line
 module.exports = fibonacci;
